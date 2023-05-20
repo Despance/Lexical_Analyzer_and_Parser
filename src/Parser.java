2671ec6590e845cb8a631366a0ad8d45c3b550ad
Despance <despance1927@hotmail.com>
2023-05-20 23:41:18 +0300

    until expression

--- src/Parser.java
+++ src/Parser.java
@@ -59,6 +59,134 @@ public class Parser {
         return true;
     }
 
+    static boolean Program() {
+        if (TopLevelForm())
+            Program();
+        return SUCCESS;
+    }
+
+    static boolean TopLevelForm() {
+        lex();
+        if (currentToken != TOKENS.LEFTPAR)
+            return FAILURE;
+        if (!SecondLevelForm())
+            return FAILURE;
+        lex();
+        if (currentToken != TOKENS.RIGHTPAR)
+            return FAILURE;
+
+        return SUCCESS;
+    }
+
+    static boolean SecondLevelForm() {
+        if (Definition()) {
+            return SUCCESS;
+        } else {
+            lex();
+            if (currentToken != TOKENS.LEFTPAR)
+                return FAILURE;
+
+            if (!FunCall())
+                return FAILURE;
+
+            lex();
+            if (currentToken != TOKENS.RIGHTPAR)
+                return FAILURE;
+
+            return SUCCESS;
+        }
+    }
+
+    static boolean Definition() {
+        lex();
+        if (currentToken != TOKENS.DEFINE)
+            return FAILURE;
+        if (!DefinitionRight())
+            return FAILURE;
+        return SUCCESS;
+    }
+
+    static boolean DefinitionRight() {
+        lex();
+        if (currentToken == TOKENS.IDENTIFIER) {
+            if (!Expression())
+                return FAILURE;
+            return SUCCESS;
+        }
+
+        if (currentToken != TOKENS.LEFTPAR)
+            return FAILURE;
+        lex();
+        if (currentToken != TOKENS.IDENTIFIER)
+            return FAILURE;
+        if (!ArgList())
+            return FAILURE;
+        // WE DONT TO LEX HERE
+        if (currentToken != TOKENS.RIGHTPAR)
+            return FAILURE;
+        if (!Statements())
+            return FAILURE;
+
+        return SUCCESS;
+
+    }
+
+    static boolean ArgList() {
+        lex();
+        if (currentToken == TOKENS.IDENTIFIER)
+            ArgList();
+        return SUCCESS;
+    }
+
+    static boolean Statements() {
+        if (Expression())
+            return SUCCESS;
+
+        if (!Definition())
+            return FAILURE;
+        if (!Statements())
+            return FAILURE;
+
+        return SUCCESS;
+
+    }
+
+    static boolean Expressions() {
+        if (Expression())
+            Expressions();
+        return SUCCESS;
+    }
+
+    static boolean Expression() {
+        lex();
+        switch (currentToken) {
+            case IDENTIFIER:
+                break;
+            case NUMBER:
+                break;
+            case CHAR:
+                break;
+            case BOOLEAN:
+                break;
+            case STRING:
+                break;
+            case LEFTPAR:
+                if (Expr()) {
+                    lex();
+                    if (currentToken != TOKENS.RIGHTPAR) {
+                        return FAILURE;
+                    }
+                } else return FAILURE;
+
+                break;
+            default:
+                return FAILURE;
+
+        }
+        return SUCCESS;
+    }
+
+
     public static boolean isBracket(char ch) {
         return ch == '(' || ch == ')' || ch == '[' || ch == ']' || ch == '{' || ch == '}';
     }
